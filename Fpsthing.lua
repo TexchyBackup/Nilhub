8084e207ab46ad9be85492a7d91ac4606ac4974a
TexchyBackup <pancececka@gmail.com>
2024-07-07 21:43:33 +0200

    Update Fpsthing.lua

--- Fpsthing.lua
+++ Fpsthing.lua
@@ -9,17 +9,17 @@ b.Text = "Toggle"
 
 local r = true
 
-local grayScreen = Instance.new("Frame")
-grayScreen.Size = UDim2.new(1, 0, 1, 0)
-grayScreen.BackgroundColor3 = Color3.new(0.5, 0.5, 0.5)
-grayScreen.ZIndex = 0 
+local gs = Instance.new("Frame")
+gs.Size = UDim2.new(1, 0, 1, 0)
+gs.BackgroundColor3 = Color3.new(0.5, 0.5, 0.5)
+gs.ZIndex = 0 
 
 b.MouseButton1Click:Connect(function()
 	r = not r
 	game:GetService("RunService"):Set3dRenderingEnabled(r)
 	if not r then
-		grayScreen.Parent = g 
+		gs.Parent = g 
 	else
-		grayScreen.Parent = nil
+		gs.Parent = nil
 	end
 end)
